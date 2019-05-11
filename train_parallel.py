3903360b05a9e367736ce0487aa529e451cfde12
amala2895 <amala1105@gmail.com>
2019-05-11 16:31:42 -0400

    merge error

--- train_parallel.py
+++ train_parallel.py
@@ -23,22 +23,22 @@ def trainp(loader, model, optimizer, epoch, cuda, log_interval, loss_func, verbo
             data1, data2, target = data1.cuda(), data2.cuda(), target.cuda()
         data1, data2, target = Variable(data1), Variable(data2), Variable(target)
 
-optimizer.zero_grad()
-output = model(data1, data2)
-loss=criterion(output,target)
+        optimizer.zero_grad()
+        output = model(data1, data2)
+        loss=criterion(output,target)
 
-loss.backward()
-optimizer.step()
-global_epoch_loss += criterion2(output, target).data
-if verbose:
-    if batch_idx % log_interval == 0:
-        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        loss.backward()
+        optimizer.step()
+        global_epoch_loss += criterion2(output, target).data
+        if verbose:
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                                                                        epoch, batch_idx * len(data1), len(loader.dataset), 100.
                                                                        * batch_idx / len(loader), loss.data))
-    global_epoch_loss=global_epoch_loss / len(loader.dataset)
+        global_epoch_loss=global_epoch_loss / len(loader.dataset)
     print('\nTrain set: Average loss: {:.4f}\n'.format(
                                                        global_epoch_loss))
-return global_epoch_loss
+    return global_epoch_loss
 
 
 def testp(loader, model, cuda,loss_func, verbose=True):
@@ -59,11 +59,11 @@ def testp(loader, model, cuda,loss_func, verbose=True):
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-    test_loss /= len(loader.dataset)
-    if verbose:
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss /= len(loader.dataset)
+        if verbose:
+            print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
                                                                                      test_loss, correct, len(loader.dataset), 100. * correct / len(loader.dataset)))
-return test_loss
+    return test_loss
 
 def valp(loader, model, cuda, loss_func,verbose=True):
     model.eval()
@@ -83,12 +83,12 @@ def valp(loader, model, cuda, loss_func,verbose=True):
 
 
 
-test_loss += criterion(output, target).data  # sum up batch loss
-pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        test_loss += criterion(output, target).data  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-test_loss /= len(loader.dataset)
-if verbose:
-    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss /= len(loader.dataset)
+        if verbose:
+            print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
                                                                                        test_loss, correct, len(loader.dataset), 100. * correct / len(loader.dataset)))
     return test_loss
